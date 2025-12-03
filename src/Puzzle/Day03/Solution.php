f05ed5b880356c597a11970e31dda48b886f867b
igorkrz <krznarigoryt@gmail.com>
2025-12-03 15:45:09 +0100

    Day 3 solution

---
+++ src/Puzzle/Day03/Solution.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Puzzle\Day03;
+
+use App\Puzzle\AbstractPuzzle;
+
+class Solution extends AbstractPuzzle
+{
+    public function solution1(array $input = []): string
+    {
+        $sum = 0;
+
+        foreach ($input as $line) {
+            $sum += (int) $this->maxAfterRemovingDigitCount($line, strlen($line) - 2);
+        }
+
+        return (string) $sum;
+    }
+
+    public function solution2(array $input = []): string
+    {
+        $sum = 0;
+        foreach ($input as $line) {
+            $sum += (int) $this->maxAfterRemovingDigitCount($line, strlen($line) - 12);
+        }
+
+        return (string) $sum;
+    }
+
+    function maxAfterRemovingDigitCount(string $input, int $digitCount): string
+    {
+        $stack = [];
+
+        foreach (str_split($input) as $d) {
+            while ($digitCount > 0 && $stack && end($stack) < $d) {
+                array_pop($stack);
+                $digitCount--;
+            }
+
+            $stack[] = $d;
+        }
+
+        if ($digitCount > 0) {
+            $stack = array_slice($stack, 0, count($stack) - $digitCount);
+        }
+
+        return implode('', $stack);
+    }
+}
